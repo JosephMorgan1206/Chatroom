f57be7769a01322ffe76123c9ef00705e0bf9ba8
josephmorgan <josephmorgan1206@gmail.com>
2023-02-04 12:59:13 +0000

    sssss

---
+++ src/utils/apiRoutes.ts
@@ -0,0 +1,11 @@
+export const host = "http://localhost:5000";
+export const connect = `${host}/api/auth/connect`;
+export const setWhale = `${host}/api/auth/setWhale`;
+export const setAvatarRoute = `${host}/api/auth/setAvatar`;
+export const setUserNameRoute = `${host}/api/auth/setUserName`;
+export const allUsersRoute = `${host}/api/auth/allUsers`;
+export const logoutRoute = `${host}/api/auth/logout`;
+export const sendMessageRoute = `${host}/api/message/addmsg`;
+export const getAllMessagesRoute = `${host}/api/message/getmsg`;
+export const getUserRoute = `${host}/api/auth/getuser`;
+export const isValidRoute = `${host}/api/auth/isValidName`;
